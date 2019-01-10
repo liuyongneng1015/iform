e6574205de10a259c3f6421c0263f2d6d715b220
liuyongneng <liuyongneng@ascs.tech>
2019-01-10 10:33:41 +0800

    '

--- icity-iform-service/src/main/java/tech/ascs/icity/iform/ICityIFormApplication.java
+++ icity-iform-service/src/main/java/tech/ascs/icity/iform/ICityIFormApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import tech.ascs.icity.jpa.service.config.EnableJPAServices;
 
 @SpringBootApplication
@@ -14,6 +18,27 @@ import tech.ascs.icity.jpa.service.config.EnableJPAServices;
 @ComponentScan("tech.ascs.icity")
 @EnableJPAServices("tech.ascs.icity")
 public class ICityIFormApplication {
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+//      corsConfiguration.addExposedHeader(HttpHeaderConStant.X_TOTAL_COUNT);
+        return corsConfiguration;
+    }
+
+    /**
+     * 跨域过滤器
+     *
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ICityIFormApplication.class, args);
