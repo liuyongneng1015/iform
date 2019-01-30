c022051d649f17f17190fac18f66e500a6c99cde
heguopei <2386824052@qq.com>
2019-01-30 10:40:33 +0800

    列表建模查询条件默认值的解析

--- icity-iform-service/src/main/java/tech/ascs/icity/iform/controller/ListModelController.java
+++ icity-iform-service/src/main/java/tech/ascs/icity/iform/controller/ListModelController.java
@@ -349,8 +349,8 @@ public class ListModelController implements tech.ascs.icity.iform.api.service.Li
 				Object defalueValue = searchItem.getSearch().getDefaultValue();
 				if(defalueValue != null && defalueValue instanceof List){
 					searchInfo.setDefaultValue(String.join(",", (List)searchItem.getSearch().getDefaultValue()));
-				} else if(defalueValue != null && defalueValue instanceof String) {
-					searchInfo.setDefaultValue(StringUtils.isEmpty(defalueValue) ? null : (String)defalueValue);
+				} else if(defalueValue != null) {
+					searchInfo.setDefaultValue(StringUtils.isEmpty(defalueValue) ? null : String.valueOf(defalueValue));
 				}
 				searchItemEntity.setOrderNo(i);
 				searchItemEntity.setSearch(searchInfo);
@@ -398,7 +398,7 @@ public class ListModelController implements tech.ascs.icity.iform.api.service.Li
 //					throw new IFormException("快速筛选必须勾选要刷选控件");
 //				}
 				if ((itemModel!=null && !StringUtils.isEmpty(itemModel.getId()) && (searchItem.getSearchValues()==null || searchItem.getSearchValues().size()==0))) {
-					throw new IFormException("快速筛选必须勾选筛选值");
+					throw new IFormException("快速筛选勾选了筛选控件后必须勾选筛选值");
 				}
                 QuickSearchEntity quickSearchEntity = new QuickSearchEntity();
 		    	if (itemModel!=null && !StringUtils.isEmpty(itemModel.getId())) {
@@ -594,7 +594,7 @@ public class ListModelController implements tech.ascs.icity.iform.api.service.Li
 						if(StringUtils.hasText(defaultValue)) {
 							ItemType itemType = searchItem.getType();
 							// Input, InputNumber返回的defaultValue是字符串格式，不是数组格式
-							if (ItemType.Input.equals(itemType) || ItemType.InputNumber.equals(itemType)) {
+							if (ItemType.Input.equals(itemType) || ItemType.InputNumber.equals(itemType) || ItemType.RadioGroup.equals(itemType)) {
 								search.setDefaultValue(defaultValue);
 							} else {
 								search.setDefaultValue(Arrays.asList(defaultValue.split(",")));
