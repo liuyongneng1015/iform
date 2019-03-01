d13190aa0de3be5629525bd17ed8795451ce22df
liuyongneng <liuyongneng@ascs.tech>
2019-03-01 15:06:42 +0800

    提交表单数据

---
+++ icity-iform-api/src/main/java/tech/ascs/icity/iform/api/model/ReferenceDataModel.java
@@ -0,0 +1,34 @@
+package tech.ascs.icity.iform.api.model;
+
+public class ReferenceDataModel{
+
+	private String tableName;
+
+	private String key;
+
+	private Boolean flag;
+
+	public String getTableName() {
+		return tableName;
+	}
+
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public Boolean getFlag() {
+		return flag;
+	}
+
+	public void setFlag(Boolean flag) {
+		this.flag = flag;
+	}
+}
