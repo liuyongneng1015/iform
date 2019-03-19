6952913f1a47b3b4dadb1b2aa08b2f3a441b5864
heguopei <heguopei@ascs.tech>
2019-03-19 15:23:47 +0800

    添加iform的feignclient包

---
+++ icity-iform-client/src/main/java/tech/ascs/icity/admin/client/FormInstanceService.java
@@ -0,0 +1,8 @@
+package tech.ascs.icity.admin.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import tech.ascs.icity.common.advice.FeignRequestInterceptor;
+
+@FeignClient(value = "iform", configuration = FeignRequestInterceptor.class)
+public interface FormInstanceService extends tech.ascs.icity.iform.api.service.FormInstanceService {
+}
