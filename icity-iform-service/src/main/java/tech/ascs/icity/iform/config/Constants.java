7fc25e40b5687eea7a76e5f3f97a7adb379fde3d
heguopei <heguopei@ascs.tech>
2019-05-05 10:36:31 +0800

    修改通过token获取用户数据的逻辑

--- icity-iform-service/src/main/java/tech/ascs/icity/iform/config/Constants.java
+++ icity-iform-service/src/main/java/tech/ascs/icity/iform/config/Constants.java
@@ -1,6 +1,8 @@
 package tech.ascs.icity.iform.config;
 
 public class Constants {
-    public static final String USERID_GET_TOKEN_PREFIX = "icity-userId-get-token-";
-    public static final String TOKEN_GET_USERINFO_PREFIX = "icity-token-get-userInfo-";
+    /** 通过token找对应的用户ID */
+    public static final String GET_USERID_BY_TOKEN = "icity-get-userId-by-token-";
+    /** 通过用户ID获取用户的数据 */
+    public static final String GET_USERINFO_BY_USERID = "icity-get-userInfo-by-userId-";
 }
