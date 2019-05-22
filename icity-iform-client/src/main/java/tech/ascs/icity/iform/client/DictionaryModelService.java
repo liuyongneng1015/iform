3d38115e58876755892ba9fc78dd776f586d5ec6
liuyongneng <liuyongneng@ascs.tech>
2019-05-22 09:33:37 +0800

    字典建模

--- icity-iform-client/src/main/java/tech/ascs/icity/iform/client/DictionaryModelService.java
+++ icity-iform-client/src/main/java/tech/ascs/icity/iform/client/DictionaryModelService.java
@@ -4,6 +4,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import tech.ascs.icity.common.advice.FeignRequestInterceptor;
 
 @FeignClient(value = "iform", configuration = FeignRequestInterceptor.class)
-public interface DictionaryModelService extends tech.ascs.icity.iform.api.service.DictionaryDataService {
+public interface DictionaryModelService extends tech.ascs.icity.iform.api.service.DictionaryModelService {
 
 }
