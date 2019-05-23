341ec2dc9189ee40f3d1bce11dd13751c545497c
heguopei <heguopei@ascs.tech>
2019-05-23 10:54:13 +0800

    修改树形下拉框的数据源

--- icity-iform-api/src/main/java/tech/ascs/icity/iform/api/model/TreeSelectDataSource.java
+++ icity-iform-api/src/main/java/tech/ascs/icity/iform/api/model/TreeSelectDataSource.java
@@ -1,13 +1,13 @@
 package tech.ascs.icity.iform.api.model;
 
 public enum TreeSelectDataSource {
-    Department("Department"),//部门
-    Position("Position"),//岗位
-    Personnel("Personnel"),//人员
+    Department("Department"),            //部门
+    Position("Position"),                //岗位
+    Personnel("Personnel"),              //人员
     PositionIdentify("PositionIdentify"),//岗位标识
-    SystemCode("SystemCode"),//系统代码
-    DictionaryModel("DictionaryModel"),//字典建模
-    Other("Other");//其他
+    DictionaryData("DictionaryData"),    //系统代码
+    DictionaryModel("DictionaryModel"),  //字典建模
+    Other("Other");                      //其他
     private String value;
     private TreeSelectDataSource(String value) {
         this.value= value;
