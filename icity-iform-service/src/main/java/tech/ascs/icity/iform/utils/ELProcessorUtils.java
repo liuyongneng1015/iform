42d4863974a148f55d8d736ee28aed5c2cf5db87
任杰 <renjie@ascs.tech>
2019-07-11 10:23:57 +0800

    feat: [APPSPTPLF-1067]针对前端的表达式出现的, undefined, !==, ===和无符号做处理

--- icity-iform-service/src/main/java/tech/ascs/icity/iform/utils/ELProcessorUtils.java
+++ icity-iform-service/src/main/java/tech/ascs/icity/iform/utils/ELProcessorUtils.java
@@ -1,7 +1,5 @@
 package tech.ascs.icity.iform.utils;
 
-import java.util.regex.Pattern;
-
 /**
  * EL处理器工具类
  *
@@ -12,18 +10,37 @@ public class ELProcessorUtils {
 
     private static final String KEY_FILTER_REGEX = "([0-9a-zA-z]{32}(?!\\s*'|\\s*\"))";
 
-    private static final Pattern PATTERN = Pattern.compile(KEY_FILTER_REGEX);
+    private static final String UNDEFINED_REGEX = "(undefined)";
+
+    private static final String ABS_EQUAL_REGEX = "(===)";
+    private static final String ABS_NO_EQUAL_REGEX = "(!==)";
 
     private static final String PREFIX = "_";
 
+    private static final String EL_PREFIX = "${";
+    private static final String EL_SUFFIX = "}";
+
     /**
-     *
      * 处理表达式中的32为的key, 对其在前面加入一个_, 预防当使用
      * 数字开头的变量的时候会报错的问题
+     *
      * @param expression 表达式
      * @return 返回加工后的表达式
      */
     public static String process(String expression) {
-        return expression.replaceAll(KEY_FILTER_REGEX, PREFIX + "$1");
+        String newExpression = expression.replaceAll(KEY_FILTER_REGEX, PREFIX + "$1")
+                .replaceAll(UNDEFINED_REGEX, "null")
+                .replaceAll(ABS_EQUAL_REGEX, "==")
+                .replaceAll(ABS_NO_EQUAL_REGEX, "!=");
+
+        if (!newExpression.trim().startsWith(EL_PREFIX)) {
+            newExpression = EL_PREFIX + newExpression;
+        }
+        if (!newExpression.trim().endsWith(EL_SUFFIX)) {
+            newExpression = newExpression + EL_SUFFIX;
+        }
+
+        return newExpression;
+
     }
 }
