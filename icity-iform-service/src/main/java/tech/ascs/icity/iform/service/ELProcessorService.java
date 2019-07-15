4bcabdcf3a1c11d2ab365b7ca5963ffa3f1ff903
任杰 <renjie@ascs.tech>
2019-07-15 09:39:34 +0800

    feat: [APPSPTPLF-1067]增加对子表子项的校验支持

--- icity-iform-service/src/main/java/tech/ascs/icity/iform/service/ELProcessorService.java
+++ icity-iform-service/src/main/java/tech/ascs/icity/iform/service/ELProcessorService.java
@@ -1,10 +1,13 @@
 package tech.ascs.icity.iform.service;
 
+import tech.ascs.icity.iform.api.model.FormDataSaveInstance;
 import tech.ascs.icity.iform.api.model.ItemInstance;
+import tech.ascs.icity.iform.model.FormModelEntity;
 import tech.ascs.icity.iform.model.FormSubmitCheckInfo;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 /**
  * 用于处理EL表达式的服务
@@ -25,4 +28,14 @@ public interface ELProcessorService {
      * @return 返回校验结果, 如果 为empty则表示校验通过, 否则里面的为错误内容
      */
     List<String> checkSubmitProcessor(Map<String, ItemInstance> itemMapping, List<FormSubmitCheckInfo> checkInfos);
+
+    /**
+     * 检查提交内容是否符合校验规则
+     *
+     * @param instance 表单实例内容
+     * @param mainEntity 主表实体类
+     * @param subModelFunction 根据id可以获取到对应子表实体类的一个Function
+     * @return 返回校验结果
+     */
+    List<String> checkSubmitProcessor(FormDataSaveInstance instance, FormModelEntity mainEntity, Function<String, FormModelEntity> subModelFunction);
 }
--- icity-iform-service/src/main/java/tech/ascs/icity/iform/utils/ELProcessorUtils.java
+++ icity-iform-service/src/main/java/tech/ascs/icity/iform/utils/ELProcessorUtils.java
@@ -15,10 +15,9 @@ public class ELProcessorUtils {
     private static final String ABS_EQUAL_REGEX = "(===)";
     private static final String ABS_NO_EQUAL_REGEX = "(!==)";
 
-    private static final String PREFIX = "_";
+    private static final String PREFIX = "#_";
 
-    private static final String EL_PREFIX = "${";
-    private static final String EL_SUFFIX = "}";
+    private static final String ARRAY_REGEX = "\\[([\\w,\\s]*)\\]";
 
     /**
      * 处理表达式中的32为的key, 对其在前面加入一个_, 预防当使用
@@ -28,19 +27,17 @@ public class ELProcessorUtils {
      * @return 返回加工后的表达式
      */
     public static String process(String expression) {
-        String newExpression = expression.replaceAll(KEY_FILTER_REGEX, PREFIX + "$1")
+
+        return expression
+                //把所有32位id替换为 #_id的形式, #表示Spel里面的变量
+                .replaceAll(KEY_FILTER_REGEX, PREFIX + "$1")
+                //把 undefined 替换为null
                 .replaceAll(UNDEFINED_REGEX, "null")
+                //把js 的 === 替换为 ==
                 .replaceAll(ABS_EQUAL_REGEX, "==")
-                .replaceAll(ABS_NO_EQUAL_REGEX, "!=");
-
-        if (!newExpression.trim().startsWith(EL_PREFIX)) {
-            newExpression = EL_PREFIX + newExpression;
-        }
-        if (!newExpression.trim().endsWith(EL_SUFFIX)) {
-            newExpression = newExpression + EL_SUFFIX;
-        }
-
-        return newExpression;
-
+                //把js 的 !== 替换为 !=
+                .replaceAll(ABS_NO_EQUAL_REGEX, "!=")
+                //把js 的数组表示方式 []  替换为 new Object[]{} 的形式
+                .replaceAll(ARRAY_REGEX, "new Object[]{$1}");
     }
 }
