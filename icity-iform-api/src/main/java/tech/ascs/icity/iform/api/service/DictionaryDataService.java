f80bf4a71ff3a4921e2316f5c9b09f7ffa0f22e3
heguopei <heguopei@ascs.tech>
2019-08-07 09:26:28 +0800

    查询下拉框数据源的时候，去掉联动解绑的linkageDataUnbind的boolean参数

--- icity-iform-api/src/main/java/tech/ascs/icity/iform/api/service/DictionaryDataService.java
+++ icity-iform-api/src/main/java/tech/ascs/icity/iform/api/service/DictionaryDataService.java
@@ -109,14 +109,12 @@ public interface DictionaryDataService {
 	@ApiImplicitParams({
 		@ApiImplicitParam(paramType="path", name = "id", value = "字典表分类ID", required = true, dataType = "String"),
 		@ApiImplicitParam(paramType="path", name = "itemId", value = "字典表选项ID", required = true, dataType = "String"),
-		@ApiImplicitParam(paramType="query", name = "itemModelId", value = "字典表选项ID", required = false, dataType = "String"),
-		@ApiImplicitParam(paramType="query", name = "linkageDataUnbind", value = "联动数据解绑", required = false, dataType = "Boolean")
+		@ApiImplicitParam(paramType="query", name = "itemModelId", value = "字典表选项ID", required = false, dataType = "String")
 	})
 	@GetMapping("/{id}/{itemId}/items")
 	List<DictionaryDataItemModel> findItems(@PathVariable(name="id", required = true) String id,
 											@PathVariable(name="itemId", required = true) String itemId,
-											@RequestParam(name="itemModelId", required = false) String itemModelId,
-											@RequestParam(name="linkageDataUnbind", defaultValue = "false") Boolean linkageDataUnbind);
+											@RequestParam(name="itemModelId", required = false) String itemModelId);
 
 
 	@ApiOperation(value = "通过批量ID获取字典表选项的详情")
